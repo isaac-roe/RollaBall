742e5cef7b5ce714466a16d1f512f56e681a72a0
Grayson Groepper <ggroepper2@gmail.com>
2022-02-17 10:29:06 -0600

    start of enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,20 +8,39 @@ public class EnemyController : MonoBehaviour
     public float lookRadius = 10f;
 
     Transform target;
-    NavMeshAgent agent;
+    UnityEngine.AI.NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
 
-        agent = GetComponent<NavMeshAgent>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                //attack
+                FaceTarget();
+            }
+        }
+    }
+
+    void FaceTarget ()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 
     void OnDrawGizmosSelected ()
