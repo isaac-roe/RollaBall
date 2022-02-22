b95ecc09566251e0cf335871e878fa2137463a8e
isaacr34 <isaac.roe@my.simpson.edu>
2022-02-21 20:44:44 -0600

    Added a basic start screen. Issue #16

--- Assets/Scripts/ButtonUI.cs
+++ Assets/Scripts/ButtonUI.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class ButtonUI : MonoBehaviour
 {
+    public void StartGame()
+    {
+        SceneManager.LoadScene("BallGame");
+    }
+
     public void NewLevelButton()
     {
         SceneManager.LoadScene("LevelTwo");
